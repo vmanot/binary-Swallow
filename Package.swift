b1f28bc4b0037255d9569997df1618a55fe7ef9e
Vatsal Manot <vatsal.manot@yahoo.com>
2024-02-28 12:51:24 -0800

    Update 1.0.9004

--- Package.swift
+++ Package.swift
@@ -3,8 +3,8 @@
 import CompilerPluginSupport
 import PackageDescription
 
-let repo = "http://github.com/vmanot/binary-Swallow/raw/1.0.9003/"
-let modules = ["Swallow", "Runtime"]
+let repo = "https://github.com/vmanot/binary-Swallow/raw/1.0.9004/"
+let modules = ["Swallow", "Runtime", "Diagnostics"]
 
 let package = Package(
   name: "Swallow",
@@ -53,12 +53,17 @@ let package = Package(
     .binaryTarget(
         name: "Swallow",
         url: repo + "Swallow.xcframework.zip",
-        checksum: "8296bee5d110cb7fedf610ce889977396d6e38df1a2efdedecfa3b375e296f77"
+        checksum: "17687734a8c0049258b002f1c0e2c5851efe1ef34d5dfcc0730dc845a17a7f5e"
     ),
     .binaryTarget(
         name: "Runtime",
         url: repo + "Runtime.xcframework.zip",
-        checksum: "213c872f535e180235b2bf005196e9233d30341330cc1fcfcc9012edbb6eca5d"
+        checksum: "a41edf2a0ae4c30451da0102559b60a11a76ef3b82e5ddf2cf3aad714dcdea6b"
+    ),
+    .binaryTarget(
+        name: "Diagnostics",
+        url: repo + "Diagnostics.xcframework.zip",
+        checksum: "5c87e98b8b057fc18ef95985f9fe7b932082e0edc95857c2b5adb5f51d16e421"
     ),
   ]
 )
